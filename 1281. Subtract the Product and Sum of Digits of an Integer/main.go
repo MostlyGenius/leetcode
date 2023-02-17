ec61d8ab5a2a0cecf83625e0807f3ecd56458cb8
Server_Head <90004142+MostlyGenius@users.noreply.github.com>
2023-02-16 21:28:37 -0800

    update main function

--- 1281. Subtract the Product and Sum of Digits of an Integer/main.go
+++ 1281. Subtract the Product and Sum of Digits of an Integer/main.go
@@ -1,15 +1,25 @@
+package main
+
+import "fmt"
+
 func subtractProductAndSum(n int) int {
-    i := 0
-    prodResult := 1
-    sumResult := 0
-    
-    for i < len(n){
-        prodResult = prodResult * n[i]
-    }
-    
-     for i < len(n){
-        sumResult = sumResult + n[i]
-    }
-    
-    return prodResult - sumResult
+	i := 0
+	prodResult := 1
+	sumResult := 0
+
+	for i < n {
+		prodResult = prodResult * n[i]
+	}
+
+	for i < len(n) {
+		sumResult = sumResult + n[i]
+	}
+
+	return prodResult - sumResult
+}
+
+func main() {
+	n := 234
+	testCase := subtractProductAndSum(n)
+	fmt.Println(testCase)
 }
